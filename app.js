50f802a26015e028e97a53dfb51517ed93a337a5
Momoka Igarashi <migarashi0318@gmail.com>
2023-07-09 15:54:06 +0200

    get method init

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+app.get("/api/v1/tasks", (req, res) => {
+    res.send("Get the all tasks");
+});
+
 app.listen(PORT, console.log("Server is started"));
