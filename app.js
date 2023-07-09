0a964b563b3978b7771a0ef92f5cfd0e05152c54
Momoka Igarashi <migarashi0318@gmail.com>
2023-07-09 15:59:47 +0200

    get method by id

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.post("/api/v1/tasks", (req, res) => {
     res.send("Created the new task");
 });
 
+app.get("/api/v1/tasks/:id", (req, res) => {
+    res.send("Got the specific task");
+});
+
 app.listen(PORT, console.log("Server is started"));
