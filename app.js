95d124594d53a75e24fea647c523760a0e7cf541
Momoka Igarashi <migarashi0318@gmail.com>
2023-07-09 15:55:57 +0200

    post method init

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.get("/api/v1/tasks", (req, res) => {
     res.send("Get the all tasks");
 });
 
+app.post("/api/v1/tasks", (req, res) => {
+    res.send("Created the new task");
+});
+
 app.listen(PORT, console.log("Server is started"));
